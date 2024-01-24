b0d19fc5be44ab9e09c60adb5740e1d400e318f7
frankjinn <frank.jin@berkeley.edu>
2024-01-24 10:26:42 -0800

    "finished hw1"

---
+++ hw1/scripts/README.txt
@@ -0,0 +1,4 @@
+Main code and training done on hw1.ipynb file. Just run the cells.
+Featurize.py also edited. Used to produce a csv of most common words used in spam and ham, then cross-checked to eliminate words with high frequency in both lists (file included). The words that were left were added as new features. Potential improvement is to just include more words from that list.
+
+Question 2e code in a seperate file named Q2.py.
--- hw1/scripts/featurize.py
+++ hw1/scripts/featurize.py
@@ -28,6 +28,10 @@ import re
 import scipy.io
 import numpy as np
 import pdb
+import pandas as pd
+import csv
+
+field_names = ['No', 'Company', 'Car Model'] 
 
 NUM_TRAINING_EXAMPLES = 4172
 NUM_TEST_EXAMPLES = 1000
@@ -115,7 +119,7 @@ def freq_record_feature(text, freq):
 def freq_out_feature(text, freq):
     return float(freq['out'])
 
-# Features that look for certain characters
+#Features that look for certain characters
 def freq_semicolon_feature(text, freq):
     return text.count(';')
 
@@ -141,6 +145,218 @@ def freq_and_feature(text, freq):
 def example_feature(text, freq):
     return int('example' in text)
 
+def freq_easy_feature(text, freq):
+    return int(freq['easy'])
+
+def freq_hundred_percent_feature(text, freq):
+    return int(freq['100%'])
+
+def freq_now_feature(text, freq):
+    return int(freq['now'])
+
+def freq_fast_feature(text, freq):
+    return int(freq['fast'])
+
+def freq_free_feature(text, freq):
+    return int(freq['free'])
+
+def freq_cash_feature(text, freq):
+    return int(freq['cash'])
+
+def freq_increase_feature(text, freq):
+    return int(freq['increase'])
+
+def freq_all_feature(text, freq):
+    return int(freq['all'])
+
+def freq_address_feature(text, freq):
+    return int(freq['address'])
+
+def freq_make_feature(text, freq):
+    return int(freq['make'])
+
+def freq_our_feature(text, freq):
+    return int(freq['our'])
+
+def freq_remove_feature(text, freq):
+    return int(freq['remove'])
+
+def freq_order_feature(text, freq):
+    return int(freq['order'])
+
+def freq_mail_feature(text, freq):
+    return int(freq['mail'])
+
+def freq_receive_feature(text, freq):
+    return int(freq['receive'])
+
+def freq_people_feature(text, freq):
+    return int(freq['people'])
+
+def freq_credit_feature(text, freq):
+    return int(freq['credit'])
+
+def freq_project_feature(text, freq):
+    return int(freq['project'])
+
+def freq_edu_feature(text, freq):
+    return int(freq['edu'])
+
+def freq_re_feature(text, freq):
+    return int(freq['re'])
+
+def freq_direct_feature(text, freq):
+    return int(freq['direct'])
+
+def freq_people_feature(text, freq):
+    return int(freq['people'])
+
+def freq_people_feature(text, freq):
+    return int(freq['our'])
+
+def freq_forward_feature(text, freq):
+    return int(freq['forward'])
+
+
+
+def freq_ect_feature(text, freq):
+    return int(freq['ect'])
+
+def freq_hou_feature(text, freq):
+    return int(freq['hou'])
+
+def freq_enron_feature(text, freq):
+    return int(freq['enron'])
+
+def freq_on_feature(text, freq):
+    return int(freq['on'])
+
+def freq_your_feature(text, freq):
+    return int(freq['your'])
+
+def freq___feature(text, freq):
+    return int(freq['_'])
+
+def freq_3_feature(text, freq):
+    return int(freq['3'])
+
+def freq_2000_feature(text, freq):
+    return int(freq['2000'])
+
+def freq_will_feature(text, freq):
+    return int(freq['will'])
+
+def freq_gas_feature(text, freq):
+    return int(freq['gas'])
+
+def freq_please_feature(text, freq):
+    return int(freq['please'])
+
+def freq_deal_feature(text, freq):
+    return int(freq['deal'])
+
+def freq_subject_feature(text, freq):
+    return int(freq['subject'])
+
+def freq_meter_feature(text, freq):
+    return int(freq['meter'])
+
+def freq_td_feature(text, freq):
+    return int(freq['td'])
+
+def freq_www_feature(text, freq):
+    return int(freq['www'])
+
+def freq_daren_feature(text, freq):
+    return int(freq['daren'])
+
+def freq_information_feature(text, freq):
+    return int(freq['information'])
+
+def freq_statement_feature(text, freq):
+    return int(freq['statement'])
+
+def freq_nbsp_feature(text, freq):
+    return int(freq['nbsp'])
+
+def freq_price_feature(text, freq):
+    return int(freq['price'])
+
+def freq_cc_feature(text, freq):
+    return int(freq['cc'])
+
+def freq_hpl_feature(text, freq):
+    return int(freq['hpl'])
+
+def freq_pm_feature(text, freq):
+    return int(freq['pm'])
+
+def freq_mmbtu_feature(text, freq):
+    return int(freq['mmbtu'])
+
+def freq_forwarded_feature(text, freq):
+    return int(freq['forwarded'])
+
+def freq_height_feature(text, freq):
+    return int(freq['height'])
+
+def freq_p_feature(text, freq):
+    return int(freq['p'])
+
+def freq_new_feature(text, freq):
+    return int(freq['new'])
+
+def freq_farmer_feature(text, freq):
+    return int(freq['farmer'])
+
+def freq_attached_feature(text, freq):
+    return int(freq['attached'])
+
+def freq_width_feature(text, freq):
+    return int(freq['width'])
+
+def freq_now_feature(text, freq):
+    return int(freq['now'])
+
+def freq_time_feature(text, freq):
+    return int(freq['time'])
+
+def freq_size_feature(text, freq):
+    return int(freq['size'])
+
+def freq_xls_feature(text, freq):
+    return int(freq['xls'])
+
+def freq_stock_feature(text, freq):
+    return int(freq['stock'])
+
+def freq_message_feature(text, freq):
+    return int(freq['message'])
+
+def freq_contract_feature(text, freq):
+    return int(freq['contract'])
+
+def freq_investment_feature(text, freq):
+    return int(freq['investment'])
+
+def freq_sitara_feature(text, freq):
+    return int(freq['sitara'])
+
+def freq_volume_feature(text, freq):
+    return int(freq['volume'])
+
+def freq_texas_feature(text, freq):
+    return int(freq['texas'])
+
+def freq_securities_feature(text, freq):
+    return int(freq['securities'])
+
+def freq_inc_feature(text, freq):
+    return int(freq['inc'])
+
+def freq_company_feature(text, freq):
+    return int(freq['company'])
+
 # Generates a feature vector
 def generate_feature_vector(text, freq):
     feature = []
@@ -150,42 +366,104 @@ def generate_feature_vector(text, freq):
     feature.append(freq_money_feature(text, freq))
     feature.append(freq_drug_feature(text, freq))
     feature.append(freq_spam_feature(text, freq))
-    feature.append(freq_prescription_feature(text, freq))
-    feature.append(freq_creative_feature(text, freq))
+    # feature.append(freq_prescription_feature(text, freq))
+    # feature.append(freq_creative_feature(text, freq))
     feature.append(freq_height_feature(text, freq))
-    feature.append(freq_featured_feature(text, freq))
-    feature.append(freq_differ_feature(text, freq))
-    feature.append(freq_width_feature(text, freq))
-    feature.append(freq_other_feature(text, freq))
-    feature.append(freq_energy_feature(text, freq))
+    # feature.append(freq_featured_feature(text, freq))
+    # feature.append(freq_differ_feature(text, freq))
+    # feature.append(freq_width_feature(text, freq))
+    # feature.append(freq_other_feature(text, freq))
+    # feature.append(freq_energy_feature(text, freq))
     feature.append(freq_business_feature(text, freq))
-    feature.append(freq_message_feature(text, freq))
-    feature.append(freq_volumes_feature(text, freq))
+    # feature.append(freq_message_feature(text, freq))
+    # feature.append(freq_volumes_feature(text, freq))
     feature.append(freq_revision_feature(text, freq))
-    feature.append(freq_path_feature(text, freq))
+    # feature.append(freq_path_feature(text, freq))
     feature.append(freq_meter_feature(text, freq))
     feature.append(freq_memo_feature(text, freq))
-    feature.append(freq_planning_feature(text, freq))
-    feature.append(freq_pleased_feature(text, freq))
-    feature.append(freq_record_feature(text, freq))
-    feature.append(freq_out_feature(text, freq))
-    feature.append(freq_semicolon_feature(text, freq))
+    # feature.append(freq_planning_feature(text, freq))
+    # feature.append(freq_pleased_feature(text, freq))
+    # feature.append(freq_record_feature(text, freq))
+    # feature.append(freq_out_feature(text, freq))
+
+    # feature.append(freq_semicolon_feature(text, freq))
     feature.append(freq_dollar_feature(text, freq))
     feature.append(freq_sharp_feature(text, freq))
     feature.append(freq_exclamation_feature(text, freq))
-    feature.append(freq_para_feature(text, freq))
-    feature.append(freq_bracket_feature(text, freq))
-    feature.append(freq_and_feature(text, freq))
+    # feature.append(freq_para_feature(text, freq))
+    # feature.append(freq_bracket_feature(text, freq))
+    # feature.append(freq_and_feature(text, freq))
 
     # --------- Add your own features here ---------
     # Make sure type is int or float
+    feature.append(freq_easy_feature(text, freq))
+    # feature.append(freq_hundred_percent_feature(text, freq))
+    # feature.append(freq_now_feature(text, freq))
+    # feature.append(freq_fast_feature(text, freq))
+    feature.append(freq_free_feature(text, freq))
+    feature.append(freq_cash_feature(text, freq))
+    # feature.append(freq_increase_feature(text, freq))
+    # feature.append(freq_all_feature(text, freq))
+    # feature.append(freq_address_feature(text, freq))
+    # feature.append(freq_make_feature(text, freq))
+    # feature.append(freq_our_feature(text, freq))
+    # feature.append(freq_remove_feature(text, freq))
+    # feature.append(freq_order_feature(text, freq))
+    # feature.append(freq_mail_feature(text, freq))
+    # feature.append(freq_receive_feature(text, freq))
+    # feature.append(freq_credit_feature(text, freq))
+    # feature.append(freq_project_feature(text, freq))
+    # feature.append(freq_edu_feature(text, freq))
+    # feature.append(freq_re_feature(text, freq))
+    # feature.append(freq_direct_feature(text, freq))
+    # feature.append(freq_people_feature(text, freq)) 
+    # feature.append(freq_our_feature(text, freq))
+    # feature.append(freq_forward_feature(text, freq))
 
+    feature.append(freq_ect_feature(text, freq))
+    feature.append(freq_hou_feature(text, freq))
+    feature.append(freq_enron_feature(text, freq))
+    feature.append(freq_on_feature(text, freq))
+    feature.append(freq_your_feature(text, freq))
+    feature.append(freq___feature(text, freq))
+    feature.append(freq_2000_feature(text, freq))
+    feature.append(freq_3_feature(text, freq))
+    feature.append(freq_will_feature(text, freq))
+    feature.append(freq_on_feature(text, freq))
+    feature.append(freq_gas_feature(text, freq))
+    feature.append(freq_please_feature(text, freq))
+    feature.append(freq_deal_feature(text, freq))
+    feature.append(freq_subject_feature(text, freq))
+    feature.append(freq_cc_feature(text, freq))
+    feature.append(freq_hpl_feature(text, freq))
+    feature.append(freq_pm_feature(text, freq))
+    feature.append(freq_will_feature(text, freq))
+    feature.append(freq_company_feature(text, freq))
+    feature.append(freq_td_feature(text, freq))
+    feature.append(freq_www_feature(text, freq))
+    feature.append(freq_daren_feature(text, freq))
+    feature.append(freq_information_feature(text, freq))
+    feature.append(freq_statement_feature(text, freq))
+    feature.append(freq_nbsp_feature(text, freq))
+    feature.append(freq_price_feature(text, freq))
+    feature.append(freq_mmbtu_feature(text, freq))
+    feature.append(freq_forwarded_feature(text, freq))
+    feature.append(freq_p_feature(text, freq))
+    feature.append(freq_new_feature(text, freq))
+    feature.append(freq_farmer_feature(text, freq))
+    feature.append(freq_attached_feature(text, freq))
+    feature.append(freq_width_feature(text, freq))
+    feature.append(freq_now_feature(text, freq))
+    feature.append(freq_time_feature(text, freq))
+    feature.append(freq_size_feature(text, freq))
+    feature.append(freq_xls_feature(text, freq))
     return feature
 
 # This method generates a design matrix with a list of filenames
 # Each file is a single training example
 def generate_design_matrix(filenames):
     design_matrix = []
+    word_freq = defaultdict(int)
     for filename in filenames:
         with open(filename, 'r', encoding='utf-8', errors='ignore') as f:
             try:
@@ -202,6 +480,17 @@ def generate_design_matrix(filenames):
             # Create a feature vector
             feature_vector = generate_feature_vector(text, word_freq)
             design_matrix.append(feature_vector)
+    # print(word_freq)
+    # if spam == 'spam':
+    #     with open('spam.csv', 'w') as csvfile:
+    #         writer = csv.DictWriter(csvfile, word_freq.keys()) 
+    #         writer.writeheader() 
+    #         writer.writerow(word_freq) 
+    # elif spam == 'ham':
+    #     with open('ham.csv', 'w') as csvfile:
+    #         writer = csv.DictWriter(csvfile, word_freq.keys()) 
+    #         writer.writeheader() 
+    #         writer.writerow(word_freq) 
     return design_matrix
 
 # ************** Script starts here **************
